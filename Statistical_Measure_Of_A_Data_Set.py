2efed07fcd1a81012165afc2291f8b8ce8bb9033
Abhi <104182911+AbhimanyuShri@users.noreply.github.com>
2024-08-16 02:17:24 -0700

    Update Statistical_Measure_Of_A_Data_Set.py

--- Statistical_Measure_Of_A_Data_Set.py
+++ Statistical_Measure_Of_A_Data_Set.py
@@ -74,7 +74,7 @@ whichmeasure()
 
 print("\nThanks for using this program!")
 
-print("\nAlso rec me so updates for this project cus I was working on this late at night so the program isn't perfect. Just abide by the instructions as the program runs or errors will appear because of format.")
+
 
 
 
